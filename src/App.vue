a4d50be5691c5b304fb47b6a5120c911a4e2649a
wryx166 <wryx166@outlook.com>
2025-02-20 11:46:35 +0800

    引入element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
-import {onMounted, ref} from 'vue'
+import {ref, computed, onMounted} from 'vue'
 
 const SpecialNames = ['沐霂'];
+const NamesList = ['lian', 'queenie', 'bekki', 'yoyi', ''];
 const NamesTable = {
   '梨安': 'lian',
-  '恬豆': 'bekki',
   '沐霂': 'queenie',
+  '恬豆': 'bekki',
   '又一': 'yoyi'
 }
+const NamesTableReverse = {
+  'lian': '梨安',
+  'queenie': '沐霂',
+  'bekki': '恬豆',
+  'yoyi': '又一'
+}
+
+const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 const dayClassToDayOfWeek = {
   'sunday': '周日',
   'monday': '周一',
@@ -18,14 +27,62 @@ const dayClassToDayOfWeek = {
   'saturday': '周六'
 };
 
-const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+function getNextName(name, direction = 1) {
+  name = name === undefined ? '' : name;
+  const index = NamesList.indexOf(name);
+  if (index === -1) {
+    throw new Error('Name not found in the list');
+  }
+  if (direction === 1) {
+    return NamesList[(index + 1) % NamesList.length];
+  } else {
+    return NamesList[(index - 1 + NamesList.length) % NamesList.length];
+  }
+}
 
-const startOfWeek = new Date('2025-02-17');
+/** @type {import('vue').Ref<Date>} */
+const startOfWeek = ref(new Date('2025-02-17'));
+
+// const value1 = ref('')
+/** @type {import('vue').ComputedRef<Date>} */
+const endOfWeek = computed(() => {
+  const end = new Date(startOfWeek.value);
+  end.setDate(startOfWeek.value.getDate() + 6);
+  return end;
+});
+
+const formatWithLeadingZero = (number) => {
+  return number.toString().padStart(2, '0');
+};
+
+const weekList = computed(() => {
+  const start = new Date(startOfWeek.value);
+  const days = [];
+  for (let i = 0; i < 7; i++) {
+    // debugger;
+    const month = formatWithLeadingZero(start.getMonth() + 1);
+    const date = formatWithLeadingZero(start.getDate());
+    const dayOfWeek = dayClassToDayOfWeek[daysOfWeek[start.getDay()]];
+    days.push({
+      date: `${month}.${date}`,
+      dayOfWeek: dayOfWeek
+    });
+    start.setDate(start.getDate() + 1);
+  }
+  return days;
+});
 
 
-const endOfWeek = new Date(startOfWeek);
-endOfWeek.setDate(startOfWeek.getDate() + 6);
-const dateRange = ref(`${startOfWeek.getFullYear()}.${startOfWeek.getMonth() + 1}.${startOfWeek.getDate()} - ${endOfWeek.getFullYear()}.${endOfWeek.getMonth() + 1}.${endOfWeek.getDate()}`);
+const dateRange = computed(() => {
+  const startYear = startOfWeek.value.getFullYear();
+  const startMonth = startOfWeek.value.getMonth() + 1;
+  const startDate = startOfWeek.value.getDate();
+  // debugger
+  const endYear = endOfWeek.value.getFullYear();
+  const endMonth = endOfWeek.value.getMonth() + 1;
+  const endDate = endOfWeek.value.getDate();
+  return `${startYear}.${startMonth}.${startDate} - ${endYear}.${endMonth}.${endDate}`;
+});
 
 // 生成随机数据的函数
 const generateRandomData = () => {
@@ -33,69 +90,145 @@ const generateRandomData = () => {
     const keys = Object.keys(NamesTable);
     const randomIndex = Math.floor(Math.random() * keys.length);
     const name = keys[randomIndex];
-    if (SpecialNames.includes(name)) {
-      return {
-        name: name,
-        class: [NamesTable[name], "special-name"]
-      };
-    }
     return {
+      class: getClassByVtuberName(name),
       name: name,
-      class: NamesTable[name]
+      visible: true
     };
   });
 };
 
-// 响应式数据
+// 特殊名字的处理
+function getClassByVtuberName(VtuberName) {
+  // debugger;
+  if (SpecialNames.includes(VtuberName)) {
+    return [NamesTable[VtuberName], "special-name"];
+  }
+  return NamesTable[VtuberName];
+}
+
 const randomData1 = ref([]);
 const randomData2 = ref([]);
+const drawer = ref(false)
+
+
+function changeVtuber(index, group, direction = 1) {
+  console.log('changeVtuber', index, group);
+  // debugger;
+
+  const data = (group === 1) ? randomData1 : randomData2;
+  const name = data.value[index].name;
+  // debugger
+  const nextName = getNextName(NamesTable[name], direction);
+
+  data.value[index] = {
+    name: NamesTableReverse[nextName],
+    class: getClassByVtuberName(NamesTableReverse[nextName]),
+    visible: true
+  };
+}
+
+// 鼠标滚轮事件
+function changeVtuberByWheelEvent(index, group, event) {
+  if (event.deltaY > 0) {
+    changeVtuber(index, group, 1);
+  } else {
+    changeVtuber(index, group, -1);
+  }
+}
+
 // 初始化时生成数据
 onMounted(() => {
   randomData1.value = generateRandomData();
   randomData2.value = generateRandomData();
 });
-
 </script>
 
 <template>
   <div id="app">
-    <time class="date-range">
+    <time class="date-range" @click="drawer = true">
       {{ dateRange }}
     </time>
-    <table>
-      <thead>
+    <table class="schedule">
+      <thead class="schedule">
       <tr>
-        <th v-for="(day, index) in daysOfWeek" :key="index">
-          <div class="day-of-week">{{ dayClassToDayOfWeek[day] }}</div>
-          <time class="day"></time>
+        <th v-for="(day, index) in weekList" :key="index" class="schedule">
+          <div class="day-of-week">{{ day.dayOfWeek }}</div>
+          <time class="day">{{ day.date }}</time>
         </th>
       </tr>
       </thead>
-      <tbody>
+      <tbody class="schedule">
       <tr>
-        <td v-for="(day, index) in daysOfWeek" :key="index"
-            :class="['with-image', day, randomData1[index]?.class]">
+        <td v-for="(day, index) in daysOfWeek"
+            :key="index"
+            :class="['with-image', day, randomData1[index]?.class]"
+            @click="changeVtuber(index, 1)"
+            @wheel="changeVtuberByWheelEvent(index, 1, $event);"
+            v-show="randomData1[index]?.visible"
+        >
           <div class="item">
-            <time class="time">21:00</time>
+            <time class="time" v-show="randomData1[index]?.name">18:30</time>
             <div class="name">{{ randomData1[index]?.name }}</div>
           </div>
         </td>
       </tr>
       <tr>
-        <td v-for="(day, index) in daysOfWeek" :key="index"
-            :class="['with-image', day, randomData2[index]?.class]">
+        <td
+            v-for="(day, index) in daysOfWeek"
+            :key="index"
+            :class="['with-image', day, randomData2[index]?.class]"
+            @click="changeVtuber(index, 2)"
+            @wheel="changeVtuberByWheelEvent(index, 2, $event);"
+            v-show="randomData2[index]?.visible"
+        >
           <div class="item">
-            <time class="time">21:00</time>
+            <time class="time" v-show="randomData2[index]?.name">21:00</time>
             <div class="name">{{ randomData2[index]?.name }}</div>
           </div>
         </td>
       </tr>
       </tbody>
     </table>
+
+
+    <el-drawer v-model="drawer" title="I'm outer Drawer" size="50%">
+      <div>
+        <div class="demo-date-picker">
+          <div class="block">
+            <el-date-picker
+                v-model="startOfWeek"
+                type="date"
+                placeholder="Pick a day"
+                :size="'default'"
+            />
+          </div>
+        </div>
+      </div>
+    </el-drawer>
+
   </div>
 </template>
 
 <style scoped>
 @import './assets/main.css';
+
+.demo-date-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
+
+.demo-date-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+
+.demo-date-picker .block:last-child {
+  border-right: none;
+}
 
 </style>
