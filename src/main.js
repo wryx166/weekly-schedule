fa18b39cdf4cdd86921a7da519e20047a18c053f
wryx166 <wryx166@outlook.com>
2025-05-25 23:23:08 +0800

    feat: 集成 Pinia，并用 Ant Design Vue 替换 Element Plus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-const app = createApp(App)
-app.mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 import tailwindcss from '@tailwindcss/vite'
 
@@ -17,11 +15,12 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
     }),
   ],
   resolve: {
